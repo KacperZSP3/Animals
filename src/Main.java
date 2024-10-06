edb04b58291270937fa369d4d872de76651c783f
KacperZSP3 <k.ksykiewicz@zsp3zamosc.pl>
2024-10-06 14:54:11 +0200

    rozbudowa interfejsu o nowe metody oraz zwierzęta

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,14 @@ interface Animal {
     default void sleep() {
         System.out.println("This animal is going to sleep.");
     }
+
+    default void eat() {
+        System.out.println("This animal is eating.");
+    }
+
+    static void endOfDay() {
+        System.out.println("The day has ended. All animals are going to sleep.");
+    }
 }
 
 class Dog implements Animal {
@@ -27,19 +35,38 @@ class Cat implements Animal {
         System.out.println("The cat is purring and going to sleep.");
     }
 }
+class Bird implements Animal {
+    public void sound() {
+        System.out.println("The bird chirps: Tweet Tweet!");
+    }
+}
+
+class Cow implements Animal {
+    public void sound() {
+        System.out.println("The cow moos: Moo Moo!");
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
         // Tworzymy obiekty klasy Dog i Cat
         Animal dog = new Dog();
         Animal cat = new Cat();
+        Animal bird = new Bird();
+        Animal cow = new Cow();
 
         // Wywołujemy metodę sound() dla psa i kota
         dog.sound();  // Wywoła metodę sound() z klasy Dog
         cat.sound();  // Wywoła metodę sound() z klasy Cat
+        bird.sound();
+        cow.sound();
 
         // Wywołujemy metodę sleep() dla psa i kota
         dog.sleep();  // Wywoła domyślną metodę sleep() z interfejsu Animal
         cat.sleep();  // Wywoła nadpisaną metodę sleep() z klasy Cat
+        bird.sleep();
+        cow.sleep();
+
+        Animal.endOfDay();
     }
 }
